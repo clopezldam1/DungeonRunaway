808edf58722f4f575aadb3778ae1768ed1d6e076
clopezldam1 <clopezldam1@gmail.com>
2023-11-11 18:46:14 +0100

    añadida luz de salida (indicador fin nivel)

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -21,7 +21,7 @@ public class player : MonoBehaviour
         //usar Update para detección de inputs:
         float moveX = Input.GetAxisRaw("Horizontal"); //devuelve -1 si le das a flecha left (or tecla "A"), devuelve 1 si le das a flecha right (or tecla "D"), devuelve 0 si no apretas ninguna de las dos
         float moveY = Input.GetAxisRaw("Vertical"); //returns 1 if up/"W", returns -1 if down/"S", returns 0 if nothing
-        moveInput = new Vector2(moveX, moveY);
+        moveInput = new Vector2(moveX, moveY).normalized;
     }
 
     //FixedUpdate se va llamando cada vez que hay un cambio en las físicas del juego (POR DEFECTO SE LLAMA 50 VECES POR SEGUNDO)
@@ -31,6 +31,10 @@ public class player : MonoBehaviour
         playerRb.MovePosition(playerRb.position + (moveInput * speed * Time.fixedDeltaTime * 2) );
         /* Pasar por parametro pos de destino (aka, pos de donde quieres moverlo):
          * A la pos actual le sumamos 1 (bc presionas tecla 1 vez) multiplicado por la velocidad en la que se mueve el personaje multiplicsdo por FixedDeltaTime para que la velocidad sea siempre la misma independientemente del rate de refesco de fixedUpdate (FixedDeltaTime te devuelve el tiempo que ha pasado desde la última llamada a fixedUpdate) multiplicado por 2 para darle mas velocidad bc iba muy lento*/
+        /* si vas en diagonal como suma el +-1 de ambas teclas, va un 40% más rápido. 
+         * Si quieres que en diagonal vaya a misma velocidad que el resto de reicciones, crea el vector del moveInput así: moveInput = new Vector2(moveX, moveY).normalized;*/
+
+
 
     }
 }
