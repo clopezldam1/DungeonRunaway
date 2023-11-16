643235b0ecab4ef0ea5ffae44507f2da22ad398a
clopezldam1 <clopezldam1@gmail.com>
2023-11-16 10:54:01 +0100

    continuado trabajando en selectSkin mainMenu -> falta cambiar nombreDragon y hacer todos los cambios de sprite en la imagen

--- Assets/Scripts/skinSelectMainMenu.cs
+++ Assets/Scripts/skinSelectMainMenu.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class nextSkinMainMenu : MonoBehaviour
 {
-    public Sprite skin;
-    public Image imagen;
-    public Canvas canvas;
-    public Button next;
-    public Button prev;
+    [SerializeField] private Image imagen;
+    [SerializeField] private Button next;
+    [SerializeField] private Button prev;
+    [SerializeField] private TextMeshProUGUI nomDragon;
 
- 
-
+    private Canvas canvas;
+    private Sprite skin;
 
 
     // Start is called before the first frame update
@@ -21,17 +23,28 @@ public class nextSkinMainMenu : MonoBehaviour
         canvas = GetComponent<Canvas>();
         skin = canvas.GetComponentInChildren<Image>().sprite; //we get the current skin 
         imagen = canvas.GetComponentInChildren<Image>();
+       
     }
 
     public void nextSkin()
     {
-        
-       // imagen.sprite = Image().sprite("Assets/Sprites/Dragon1/Attack_017.png");
+
+        nomDragon.text = "aaaa";
+        nomDragon.text = skin.GetSpriteID().ToString(); //esto peta
+
+        nomDragon.text = "bbbbb"; //aqui no llega
+
+        // imagen.sprite = Image().sprite("Assets/Sprites/Dragon1/Attack_017.png");
     }
 
     public void previousSkin()
     {
 
         imagen.sprite = skin;
+    }
+
+    public void confirmSelect()
+    {
+
     }
 }
