94938f3941e22357d99ee257f97d80f19d3e3f40
clopezldam1 <clopezldam1@gmail.com>
2023-11-19 18:47:59 +0100

    intentado incorporar sfx al hacer click en botones, no logrado.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,11 +5,22 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource srcBgMusic;
     [SerializeField] AudioSource srcSfx;
     public AudioClip bgMusic;
-    public AudioClip sfx1;
+    public AudioClip[] sfx;
 
     private void Start()
     {
         srcBgMusic.clip = bgMusic;
         srcBgMusic.Play();
+    }
+
+    public void PlaySfx(AudioClip sfxClip) 
+    {
+        //reproducir solo 1 vez
+        srcSfx.PlayOneShot(sfxClip);
+    }
+    public void PlaySfx(int sfxClipIndex)
+    {
+        //reproducir solo 1 vez
+        srcSfx.PlayOneShot(sfx[sfxClipIndex]);
     }
 }
